f6631cdc05784b8b505b21c9f678e1a07be90bfb
dianahoppy <dianahopkinson@mmc.school.nz>
2025-06-03 09:40:19 +1200

    partr

--- C_04_string_check_v2.py
+++ C_04_string_check_v2.py
@@ -1,12 +1,13 @@
 # Functions go here
-def string_check(question, valid_ans_list, num_letters):
+def string_check(question, valid_answers=('yes', 'no'),
+                 num_letters=1):
     """Checks that users enter the full word
     or the 'n' letter/s of a word from a list of valid responses"""
 
     while True:
         response = input(question).lower()
 
-        for item in valid_ans_list:
+        for item in valid_answers:
 
             # check if the response is the entire word
             if response == item:
@@ -16,12 +17,12 @@ def string_check(question, valid_ans_list, num_letters):
             elif response == item[:num_letters]:
                 return item
 
-        print(f"Please choose an option from {valid_ans_list}")
+        print(f"Please choose an option from {valid_answers}")
 
 # Main routine goes here
-yes_no_list = ['yes', 'no']
 payment_list = ('cash', 'credit')
-like_coffee = string_check("Do you like coffee? ",yes_no_list, 1)
-print(f"You choose {like_coffee}")
+
+want_instructions = string_check("Do you want to see the instructions? ")
+print(f"You choose {want_instructions}")
 pay_method = string_check("Payment method ", payment_list, 2)
 print(f"You chose {pay_method}")
